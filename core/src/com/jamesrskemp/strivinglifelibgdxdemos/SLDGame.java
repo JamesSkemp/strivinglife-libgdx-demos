fce7949b8a1e570c7456393ad3b788d190f39cd4
James Skemp <strivinglife@gmail.com>
2015-07-19 17:33:40 -0500

    Switch main class over to a game, so we can have multiple screens.

--- core/src/com/jamesrskemp/strivinglifelibgdxdemos/SLDGame.java
+++ core/src/com/jamesrskemp/strivinglifelibgdxdemos/SLDGame.java
@@ -1,12 +1,12 @@
 package com.jamesrskemp.strivinglifelibgdxdemos;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SLDGame extends ApplicationAdapter {
+public class SLDGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
